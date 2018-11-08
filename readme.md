15be40bffd184152e997f54882948369c91829cf
WrBug <i@wrbug.com>
2018-11-08 15:38:41 +0800

    update

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 ![](https://i.loli.net/2018/11/08/5be3e30ce5aa2.png)
+
+![](https://i.loli.net/2018/11/08/5be3e55b69ae6.png)
--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,7 @@ import java.lang.reflect.Method;
 public class Test {
     public static void main(String[] args) {
         try {
+            //该文件需要添加一个Artifacts，build后生成
             ClassLoader classLoader = ClassLoaderUtil.loadJarFile(new File("out/artifacts/charles_crack_jar/charles-crack.jar"));
             String name = "wrbug";
             String key = "ss";
@@ -13,14 +14,21 @@ public class Test {
             Constructor<?> declaredConstructor = kKpk.getDeclaredConstructor(String.class, String.class);
             declaredConstructor.setAccessible(true);
             Object obj = declaredConstructor.newInstance(name, key);
+            System.out.println("破解成功");
+
+            System.out.println("检查JZlU方法");
             Field jZlU = kKpk.getDeclaredField("JZlU");
             jZlU.setAccessible(true);
             System.out.println(jZlU.get(obj));
             System.out.println(name.replaceAll("[ \u1680\u180e\u2000\u200a\u202f\u205f\u3000]", " "));
+
+            System.out.println("检查KcPF方法");
             Method method = kKpk.getDeclaredMethod("KcPF", String.class, String.class, int.class);
             method.setAccessible(true);
             Object v1 = method.invoke(obj, name, key, 4);
             System.out.println(v1);
+
+            System.out.println("检查lcJx方法");
             Method lcJx = kKpk.getDeclaredMethod("lcJx", long.class);
             lcJx.setAccessible(true);
             Object v2 = lcJx.invoke(obj, v1);
